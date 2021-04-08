d5af6c4f3deb18caba816dba16022d69dd80e76e
Ray Hsu <ray199185@gmail.com>
2021-04-08 13:02:18 +0800

    create funtion

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
 	def index
-		
+		@restaurants = Restaurant.all
 	end
 
 	def new
@@ -8,6 +8,17 @@ class RestaurantsController < ApplicationController
 	end
 
 	def create
-		
+		@restaurant = Restaurant.new(restaurant_params)
+
+		if @restaurant.save
+			redirect_to restaurants_path, notice: "新增餐廳成功"
+		else
+			render :new
+		end
 	end
+
+	private
+		def restaurant_params
+			params.require(:restaurant).permit(:title, :tel, :email, :address, :description)
+		end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+  validates :title, presence: true
 end
