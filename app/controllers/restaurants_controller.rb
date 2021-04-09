75f8c3bba9bdfa09dc2ccf7062d3c65bbe1b0212
Ray Hsu <ray199185@gmail.com>
2021-04-09 09:51:11 +0800

    CRUD finished

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,11 @@
 class RestaurantsController < ApplicationController
+	before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@restaurants = Restaurant.all
+	end
+
+	def show
 	end
 
 	def new
@@ -11,13 +16,33 @@ class RestaurantsController < ApplicationController
 		@restaurant = Restaurant.new(restaurant_params)
 
 		if @restaurant.save
-			redirect_to restaurants_path, notice: "新增餐廳成功"
+			redirect_to restaurants_path
 		else
 			render :new
 		end
 	end
 
+	def edit
+	end
+
+	def update
+		if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
 	private
+		def find_restaurant
+			@restaurant = Restaurant.find(params[:id])
+		end
+
 		def restaurant_params
 			params.require(:restaurant).permit(:title, :tel, :email, :address, :description)
 		end
