6cc577a91a6208150e97dd0954c77aeb1466b706
Ray Hsu <ray199185@gmail.com>
2021-04-12 15:18:12 +0800

    WIP - create users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create] do
+    collection do
+      get :sign_up, action: 'new'
+    end
+  end
+
   resources :restaurants
+
+  root 'restaurants#index'
+
+  # get '/restaurants', to: 'restaurants#index'
+  # post '/restaurants', to: 'restaurants#create'
+  # get '/new_restaurant', to: 'restaurants#new'
+  # post '/restaurant_list', to: 'restaurants#list'
 end
