5f8a78657e256dc6230c951be7eecb97928e5d7a
Ray Hsu <ray199185@gmail.com>
2021-04-12 14:08:19 +0800

    add column and update restaurant.rb

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,14 @@
 class Restaurant < ApplicationRecord
   validates :title, presence: true
+
+  default_scope { available }
+  scope :available, -> { where(deleted_at: nil) }
+
+  def destroy
+    update(deleted_at: Time.now)
+  end
 end
+
+
+
+  
