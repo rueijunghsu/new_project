9b76835f696ff21dbb8c8f37bf315ef9280ff4ad
Ray Hsu <ray199185@gmail.com>
2021-04-19 13:57:30 +0800

    user sign in / out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,25 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if User.login(user_params)
+    user = User.login(user_params)
+
+    if user
       # 發號碼牌
+      session[:thankyou9527] = user.id
+      # setcookie
+
       # 轉去首頁
       redirect_to root_path
     else
       redirect_to sign_in_sessions_path
     end
+  end
+
+  def destroy
+    # 撕票
+    session[:thankyou9527] = nil
+    # 轉
+    redirect_to root_path
   end
 
   private
